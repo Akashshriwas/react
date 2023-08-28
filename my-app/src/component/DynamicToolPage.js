d221f6e85996e1925fa442cc4e68cc986724d3c3
akash <akashshriwas08@gmail.com>
2023-08-28 15:29:55 +0530

    commit

--- my-app/src/component/DynamicToolPage.js
+++ my-app/src/component/DynamicToolPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import SS from './SS'
+import React, { useState } from 'react';
 
-export default function DynamicToolPage() {
+const DynamicToolPage= () => {
+  const [selectedTool, setSelectedTool] = useState('');
+
+  const handleToolChange = (event) => {
+    setSelectedTool(event.target.value);
+  };
+
   return (
     <div>
-      <SS.js/>
-
+      <h2>Select a Dynamic Tool</h2>
+      <select value={selectedTool} onChange={handleToolChange}>
+        <option value="">Select a tool</option>
+        <option value="mobsf">Mobsf</option>
+        <option value="drozer">Drozer</option>
+        <option value="drozer">Quark-Engine</option>
+        {/* Add more tool options here */}
+      </select>
+      {selectedTool && (
+        <div>
+          <h3>Selected Tool: {selectedTool}</h3>
+          {/* Render tool-specific content here */}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default DynamicToolPage;
+
